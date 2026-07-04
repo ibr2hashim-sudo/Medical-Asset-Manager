e173c9afdb4143b42bbc95f0efe0603f72742a7f
ibr2hashim-sudo <ibr2hashim@gmail.com>
2026-07-04 05:56:31 +0300

    Update AppConstants.kt

--- app/src/main/java/com/example/ui/AppConstants.kt
+++ app/src/main/java/com/example/ui/AppConstants.kt
@@ -1,1 +1,13 @@
+package com.example.ui
+
+// تعريف الثوابت التي يشتكي منها الـ Compiler
+object AppConstants {
+    const val StatusWorking = "شغال"
+    const val StatusDamaged = "تالف"
+    const val HealthAndSafety = "السلامة والصحة"
+    const val FileDownload = "تحميل"
+    const val FileUpload = "رفع"
+    const val Translate = "ترجمة"
+    const val PostAdd = "إضافة"
+}
 
