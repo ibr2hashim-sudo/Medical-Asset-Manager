0344a32706b56a599e5087556ac75545cccdbf03
ibr2hashim-sudo <ibr2hashim@gmail.com>
2026-07-04 06:52:20 +0300

    Update Gradle plugins and dependencies versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp") // تعريف إضافة ksp
+    id("com.android.application") version "8.2.0"
+    id("org.jetbrains.kotlin.android") version "1.9.0"
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13"
 }
 
 android {
